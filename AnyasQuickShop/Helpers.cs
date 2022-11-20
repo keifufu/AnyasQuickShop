d16798e1ff661ec4b70629645ec4742fd265cbc4
keifufu <github@keifufu.dev>
2022-11-20 01:20:33 +0100

    v0.4.5

---
+++ AnyasQuickShop/Helpers.cs
@@ -0,0 +1,14 @@
+﻿namespace AnyasQuickShop
+{
+    internal class Helpers
+    {
+        public void UnmountAllBodyParts(CarLoader carLoader)
+        {
+            foreach (CarPart part in carLoader.carParts)
+            {
+                if (part.name != "body" && part.name != "details")
+                    carLoader.TakeOffCarPart(part.name);
+            }
+        }
+    }
+}
